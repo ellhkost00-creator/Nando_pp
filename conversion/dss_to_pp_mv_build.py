2774bcdb8d41b18e75c59738aa100ff3ea9f2905
antoniskost00 <antonis.kost00@gmail.com>
2026-04-11 12:20:11 +0300

    modified:   conversion/dss_to_pp_mv_build.py

--- conversion/dss_to_pp_mv_build.py
+++ conversion/dss_to_pp_mv_build.py
@@ -726,11 +726,12 @@ def build_mv_regulators_from_dss_one_per_set(
             except: pass
 
         # sn_mva: override > DSS kva > fallback
+        # NOTE: regulator stub kVA values are already in MVA units in the DSS file
         if base in sn_mva_overrides:
             sn_mva = float(sn_mva_overrides[base])
         else:
             kva = wdg.get(1, {}).get("kva") or wdg.get(2, {}).get("kva")
-            sn_mva = (float(kva) / 1000.0) if kva else 1.0
+            sn_mva = float(kva) if kva else 1.0
 
         tid = pp.create_transformer_from_parameters(
             net,
